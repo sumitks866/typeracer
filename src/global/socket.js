caf80f9e1181e7232bc4d4fc8043bb59cd98b790
sumitks866 <31448420+sumitks866@users.noreply.github.com>
2024-01-07 23:17:00 +0530

    Update socket.js

--- src/global/socket.js
+++ src/global/socket.js
@@ -1,4 +1,4 @@
 import socketIOClient from 'socket.io-client'
 
-export const endpoint = 'https://typeracer-server.herokuapp.com/'
+export const endpoint = 'https://typeracer-server-engq.onrender.com'
 export const socket = socketIOClient(endpoint,{transports: ['websocket', 'polling', 'flashsocket']})
